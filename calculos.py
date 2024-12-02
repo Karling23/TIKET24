c034c7f668a3d189eedaafb3ccd6987d011655e0
Aliii666 <aliyhamy04@gmail.com>
2024-12-02 11:47:02 -0500

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -1,2 +1,7 @@
 def encontrarMenor(*num):
     return min(num)
+
+def calcularCuota(monto, tasa_interes, meses):
+    tasa_mensual = tasa_interes / 100 / 12  
+    cuota = monto * (tasa_mensual * (1 + tasa_mensual) ** meses) / ((1 + tasa_mensual) ** meses - 1) 
+    return cuota
